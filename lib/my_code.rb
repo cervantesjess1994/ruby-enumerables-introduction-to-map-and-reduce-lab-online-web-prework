67da2de7b25ad3a961292c53416fbbb46eca7288
Jessica Cervantes <cervantesjess1994@gmail.com>
2019-08-07 10:23:17 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,15 @@
-# My Code here....
+def map_to_negativize(source_array)
+  array - source
+end
+
+def map_to_no_change(source_array)
+
+end
+
+def map_to_double(source_array)
+
+end
+
+def map_to_square(source_array)
+
+end 
